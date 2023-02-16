87e2617c4743f38b7482d2218fdda3c9ebc54509
Hamilton Junior <hamilton-souza@outlook.com.br>
2023-02-16 19:24:09 -0300

    created basic structure of the slide, added functionality to change slides manually and automatically

---
+++ src/Slide.ts
@@ -0,0 +1,71 @@
+import Timeout from "./TimeOut.js";
+
+export default class Slide {
+  container;
+  slides;
+  controls;
+  time;
+  index;
+  slide;
+  timeout: Timeout | null;
+  constructor(
+    container: Element,
+    slides: Element[],
+    controls: Element,
+    time: number = 500
+  ) {
+    this.container = container;
+    this.slides = slides;
+    this.controls = controls;
+    this.time = time;
+    this.index = 0;
+    this.slide = this.slides[this.index];
+    this.timeout = null;
+
+    this.init();
+  }
+
+  hide(el: Element) {
+    el.classList.remove("active");
+  }
+  show(index: number) {
+    this.index = index;
+    this.slide = this.slides[this.index];
+    this.slides.forEach((slide) => {
+      this.hide(slide);
+    });
+    this.slide.classList.add("active");
+    this.auto(this.time);
+  }
+
+  auto(time: number) {
+    this.timeout?.clear();
+    this.timeout = new Timeout(() => this.next(), time);
+  }
+
+  prev() {
+    const prev = this.index - 1 < 0 ? 0 : this.index - 1;
+    this.show(prev);
+  }
+
+  next() {
+    const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+    this.show(next);
+  }
+
+  private addControls() {
+    const prevButton = document.createElement("button");
+    const nextButton = document.createElement("button");
+    prevButton.innerText = "Slide Anterior";
+    nextButton.innerText = "Próximo Slide";
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
+    prevButton.addEventListener("pointerup", () => this.prev());
+    nextButton.addEventListener("pointerup", () => this.next());
+  }
+
+  private init() {
+    this.addControls();
+    this.show(this.index);
+  }
+}
